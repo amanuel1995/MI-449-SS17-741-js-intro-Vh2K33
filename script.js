145619fa8add701065d3423cbf85135e0c26be8f
Amanuel <amaniye1@live.com>
2017-01-31 18:10:15 -0500

    JS audio replaced with HTML Audio Element and all JS moved to script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // initialize variable
-var buttonOne,buttonTwo, buttonThree, audio;
-//define functions
-function Boom() {
-  audio = new Audio();
-  audio.src = "drum-sounds/boom.wav";
-  audio.play();
+var buttonOne, buttonTwo, buttonThree, audio
+// define functions for each sound to be played
+function Boom () {
+  audio = document.getElementById('boom')
+  audio.play()
 };
-function Clap() {
-  audio = new Audio();
-  audio.src = "drum-sounds/clap.wav";
-  audio.play();
+function Clap () {
+  audio = document.getElementById('clap')
+  audio.play()
 };
-function Tink() {
-  audio = new Audio();
-  audio.src = "drum-sounds/tink.wav";
-  audio.play();
+function Tink () {
+  audio = document.getElementById('tink')
+  audio.play()
 };
+// geting elements by ID
+buttonOne = document.getElementById('first')
+buttonTwo = document.getElementById('second')
+buttonThree = document.getElementById('third')
+// Event handlers to play respective sound onclick & mouseenter
+buttonOne.addEventListener('click', Boom)
+buttonOne.addEventListener('mouseenter', Boom)
+buttonTwo.addEventListener('click', Clap)
+buttonTwo.addEventListener('mouseenter', Clap)
+buttonThree.addEventListener('click', Tink)
+buttonTwo.addEventListener('mouseenter', Tink)
