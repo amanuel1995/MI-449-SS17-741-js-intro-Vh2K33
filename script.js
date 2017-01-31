9a51bcd623682955e7afe429666a8178208dcd82
Amanuel <amaniye1@live.com>
2017-01-31 14:47:47 -0500

    removed absolute pathnames

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ var buttonOne,buttonTwo, buttonThree, audio;
 //define functions
 function Boom() {
   audio = new Audio();
-  audio.src = "/drum-sounds/boom.wav";
+  audio.src = "drum-sounds/boom.wav";
   audio.play();
 };
 function Clap() {
   audio = new Audio();
-  audio.src = "/drum-sounds/clap.wav";
+  audio.src = "drum-sounds/clap.wav";
   audio.play();
 };
 function Tink() {
   audio = new Audio();
-  audio.src = "/drum-sounds/tink.wav";
+  audio.src = "drum-sounds/tink.wav";
   audio.play();
 };
